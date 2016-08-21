c1afa4cbf912b72a02fd54b77304ebfdb63ef8ab
MalabarTakemura <malabar27@mail.ru>
2016-08-21 21:38:42 +0300

    Добавлена бесконечная прокрутка

--- js/slider.js
+++ js/slider.js
@@ -14,16 +14,16 @@ $(document).ready(function() {
 	(function($){ 
 		$.fn.mt_line_slider = function(options){
 			options = $.extend({
-				buttonLeft : "#btn-left", 		// кнопка влево
-				buttonRight : "#btn-right",		// кнопка вправо
-				countVisibleItem : 8,			// колличество видимых элементов 
+				button_left : "#btn-left", 		// кнопка влево
+				button_right : "#btn-right",		// кнопка вправо
+				count_visible_item : 8,			// колличество видимых элементов 
 				item : ".item"					// элемент слайдера
     		}, options);
 
 			var p_This = $(this);							//укатель на слайдер
 			var width = $(this).width();
-			var itemCount = $(this).children(options.item).length;
-			var offset = itemCount * width;
+			var item_count = $(this).children(options.item).length;
+			var offset = item_count * width;
 			
 			$(this).css('width',offset);
 			offset = 0;
@@ -46,25 +46,33 @@ $(document).ready(function() {
 
 			// Обработчики событий
 		    var i=0;
-			$(options.buttonRight).click(function(){	
-				if (i< (itemCount - options.countVisibleItem)) {	
+			$(options.button_right).click(function(){	
+				if (i< (item_count - options.count_visible_item)) {	
 					offset += item_w;
-					p_This.css({
-						"transform":"translate3d(-"+offset+"px, 0px, 0px)",
-						"transition":"all 0.3s ease"});
 					i++;
 				}
+				else{
+					i=1;
+					offset=0;
+				}
+				p_This.css({
+					"transform":"translate3d(-"+offset+"px, 0px, 0px)",
+					"transition":"all 0.3s ease"});
 			});
 
 
-			$(options.buttonLeft).click(function(){
+			$(options.button_left).click(function(){
 				if (i>0) { 
 					offset -= item_w;
-					p_This.css({
-						"transform":"translate3d(-"+offset+"px, 0px, 0px)", 
-						"transition":"all 0.3s ease;"});
 					i--;
 				}
+				else{
+					i = item_count - options.count_visible_item;
+					offset = item_w*i;
+				}
+				p_This.css({
+					"transform":"translate3d(-"+offset+"px, 0px, 0px)", 
+					"transition":"all 0.3s ease;"});
 			});
 			
 			return this;
